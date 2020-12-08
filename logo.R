947eccaa1eb343b3bd690571dfc992baf9c377f4
Koen Hufkens <koen.hufkens@gmail.com>
2020-12-09 00:49:24 +0100

    logo

--- logo.R
+++ logo.R
@@ -1,9 +1,10 @@
 library(hexSticker)
 
 sticker(
-  "line.png",
+  "laser_white.png",
   package="diffabsr",
         filename="logo.png",
-  p_size=20, s_x=1, s_y=.75, s_width=.6,
+  #p_size=20,
+  s_x=1, s_y=.75, s_width=.6,
   h_color = "#1881C2"
   )
